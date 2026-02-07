94c8e3ead543f2d735415566992ffa768f3795a0
Shailesh Ajabe <Shaileshajabe2013@gmail.com>
2026-02-07 11:08:53 +0530

    firstOccurence

---
+++ src/firstOccurence.java
@@ -0,0 +1,13 @@
+public class firstOccurence {
+    int h=0;
+    public int firstOccur(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    return arr[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
